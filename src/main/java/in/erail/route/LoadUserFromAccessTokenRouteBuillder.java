2a4a26c2d1fa99516dd3ad4a5377b3ed5b7f9d13
Vinay <v@v.com>
2018-02-26 18:24:57 +0530

    Migrated

--- src/main/java/in/erail/route/LoadUserFromAccessTokenRouteBuillder.java
+++ src/main/java/in/erail/route/LoadUserFromAccessTokenRouteBuillder.java
@@ -3,8 +3,8 @@ package in.erail.route;
 import com.google.common.base.Strings;
 import com.google.common.net.HttpHeaders;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.oauth2.impl.AccessTokenImpl;
 import io.vertx.ext.auth.oauth2.impl.OAuth2AuthProviderImpl;
+import io.vertx.ext.auth.oauth2.impl.OAuth2TokenImpl;
 import io.vertx.reactivex.ext.auth.oauth2.AccessToken;
 import io.vertx.reactivex.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.reactivex.ext.web.Router;
@@ -41,7 +41,7 @@ public class LoadUserFromAccessTokenRouteBuillder extends AbstractRouterBuilderI
         OAuth2AuthProviderImpl provider = (OAuth2AuthProviderImpl) getOAuth2Auth().getDelegate();
         JsonObject accessToken = new JsonObject().put("access_token", access_token.split(" ")[1]);
         try {
-          AccessTokenImpl token = new AccessTokenImpl(provider, accessToken);
+          OAuth2TokenImpl token = new OAuth2TokenImpl(provider, accessToken);
           pRoutingContext.setUser(new AccessToken(token));
         } catch (RuntimeException e) {
           getLog().error(e);
