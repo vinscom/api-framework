ab9fa14bb18f49158e2e1f14c6df8a8cd56fc417
Vinay Kumar <v@v.com>
2019-01-06 23:33:51 +0530

    Fixed cross

--- src/test/java/in/erail/route/CORSRouteBuilderTest.java
+++ src/test/java/in/erail/route/CORSRouteBuilderTest.java
@@ -12,6 +12,7 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.Rule;
 import in.erail.glue.Glue;
 import io.netty.handler.codec.http.HttpHeaderNames;
+import io.vertx.core.http.HttpMethod;
 
 /**
  *
@@ -41,7 +42,7 @@ public class CORSRouteBuilderTest {
             .putHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "cache-control,content-type,postman-token")
             .handler(response -> {
               context.assertEquals(response.statusCode(), 200, response.statusMessage());
-              context.assertEquals(response.getHeader(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString()), "GET,POST,OPTIONS");
+              context.assertEquals(response.getHeader(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString()), "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT");
               context.assertEquals(response.getHeader(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()), "*");
               context.assertEquals(response.getHeader(HttpHeaderNames.ACCESS_CONTROL_MAX_AGE.toString()), "3600");
               context.assertEquals(response.getHeader(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString()), "X-POST");
