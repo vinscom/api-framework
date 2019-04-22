5c95343b67b0542de2ede99869b46dc5ba6ec077
Vinay Kumar <v@v.com>
2019-04-22 16:10:30 +0100

    Added Helper class to manage entity manager

---
+++ src/main/java/in/erail/persistence/EntityManagerHelper.java
@@ -0,0 +1,52 @@
+package in.erail.persistence;
+
+import io.reactivex.Single;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author vinay
+ */
+public class EntityManagerHelper {
+
+  private EntityManagerFactory mEntityManagerFactory;
+  private Logger mLog;
+
+  public Single<EntityManager> getEM() {
+    return Single.using(() -> getEntityManagerFactory().createEntityManager(), em -> Single.just(em), em -> em.close(), false);
+  }
+
+  public Single<EntityManager> getEMTx() {
+    return Single.using(() -> getEntityManagerFactory().createEntityManager(), em -> {
+      em.getTransaction().begin();
+      return Single.just(em);
+    }, em -> {
+      EntityTransaction tx = em.getTransaction();
+      if (tx.isActive()) {
+        getLog().error("Resource leakage: Entity Manager tx not finished. Rolling back tx");
+        tx.rollback();
+      }
+      em.close();
+    }, false);
+  }
+
+  public Logger getLog() {
+    return mLog;
+  }
+
+  public void setLog(Logger pLog) {
+    this.mLog = pLog;
+  }
+
+  public EntityManagerFactory getEntityManagerFactory() {
+    return mEntityManagerFactory;
+  }
+
+  public void setEntityManagerFactory(EntityManagerFactory pEntityManagerFactory) {
+    this.mEntityManagerFactory = pEntityManagerFactory;
+  }
+
+}
