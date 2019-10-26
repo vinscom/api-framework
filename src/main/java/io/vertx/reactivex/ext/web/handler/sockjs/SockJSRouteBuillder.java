6fe2b09c402eefc7c497f29d864c7b11285ca35f
Vinay Kumar <v@v.com>
2019-10-26 16:28:11 +0530

    Fixed SockJS

--- src/main/java/io/vertx/reactivex/ext/web/handler/sockjs/SockJSRouteBuillder.java
+++ src/main/java/io/vertx/reactivex/ext/web/handler/sockjs/SockJSRouteBuillder.java
@@ -1,6 +1,5 @@
 package io.vertx.reactivex.ext.web.handler.sockjs;
 
-import in.erail.glue.annotation.StartService;
 import in.erail.route.AbstractRouterBuilderImpl;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
@@ -12,27 +11,10 @@ import io.vertx.reactivex.ext.web.Router;
  *
  * @author vinay
  */
-public class SockJSRouteBuillder  extends AbstractRouterBuilderImpl {
+public class SockJSRouteBuillder extends AbstractRouterBuilderImpl {
 
   private BridgeOptions mBridgeOptions;
   private Handler<BridgeEvent> mBridgeEventHandler;
-  private Router mSockJSRouter;
-
-  @StartService
-  public void start() {
-
-    // Eventbus handle
-    SockJSHandlerOptions sockJSHandlerOptions = (new SockJSHandlerOptions())
-            .addDisabledTransport(Transport.EVENT_SOURCE.toString())
-            .addDisabledTransport(Transport.HTML_FILE.toString())
-            .addDisabledTransport(Transport.JSON_P.toString())
-            .addDisabledTransport(Transport.XHR.toString())
-            .setInsertJSESSIONID(false);
-
-    mSockJSRouter = SockJSHandler
-            .create(getVertx(), sockJSHandlerOptions)
-            .bridge(getBridgeOptions(), getBridgeEventHandler());
-  }
 
   public BridgeOptions getBridgeOptions() {
     return mBridgeOptions;
@@ -52,7 +34,17 @@ public class SockJSRouteBuillder  extends AbstractRouterBuilderImpl {
 
   @Override
   public Router getRouter(Router pRouter) {
-    return mSockJSRouter;
+    // Eventbus handle
+    SockJSHandlerOptions sockJSHandlerOptions = (new SockJSHandlerOptions())
+            .addDisabledTransport(Transport.EVENT_SOURCE.toString())
+            .addDisabledTransport(Transport.HTML_FILE.toString())
+            .addDisabledTransport(Transport.JSON_P.toString())
+            .addDisabledTransport(Transport.XHR.toString())
+            .setInsertJSESSIONID(false);
+
+    return SockJSHandler
+            .create(getVertx(), sockJSHandlerOptions)
+            .bridge(getBridgeOptions(), getBridgeEventHandler());
   }
 
 }
